22a70009800bc93e01e5b9e51d29cdb9d7d7f58d
Bruno Coimbra <bbcoimbra@gmail.com>
2012-04-06 15:41:31 -0300

    tests adjust

---
+++ test/test_helper.rb
@@ -0,0 +1,3 @@
+require 'minitest/spec'
+$:.push File.expand_path(File.join(File.dirname(__FILE__), '..', 'lib'))
+$:.push File.expand_path(File.dirname(__FILE__))
---
+++ test/unit/formater_test.rb
@@ -0,0 +1,6 @@
+require '../test_helper'
+require 'i18n/tools/formater'
+
+describe I18n::Tools::Formater do
+
+end
