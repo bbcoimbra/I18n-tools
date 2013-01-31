e3a2da5750093bd938b85c9e4a92c704a6bb556f
Bruno Coimbra <bbcoimbra@gmail.com>
2013-01-31 01:38:18 -0200

    added StringConverter

--- lib/i18n/tools/string_converter.rb
+++ lib/i18n/tools/string_converter.rb
@@ -9,7 +9,45 @@ module I18n
       end
 
       def to_h
-        {"a" => "b"}
+        h = Hash.new{|hash, key| hash[key] = {}}
+        lines = @string.split("\n")
+        lines.each do |line|
+          str_keys, value = line.split("\t")
+          keys = str_keys.split(".")
+          store(h, keys, value)
+        end
+        h
+      end
+
+      private
+
+      def store(hash, keys, value)
+        if keys.size == 1
+          hash[keys.first] = format(value)
+        else
+          ks = keys.dup
+          key = ks.shift
+          h = hash[key]
+          if h.nil?
+            h = {}
+            hash[key] = h
+          end
+          store(h, ks, value)
+        end
+      end
+
+      def format(value)
+        if value =~ /^\[/
+          parse_array(value)
+        else
+          value
+        end
+      end
+
+      def parse_array(value)
+        value.sub!('["','').sub!('"]','')
+        value.gsub!("\", \"", "\",\"")
+        value.split("\",\"")
       end
 
     end
--- test/unit/i18n/tools/string_converter_test.rb
+++ test/unit/i18n/tools/string_converter_test.rb
@@ -10,4 +10,25 @@ describe I18n::Tools::StringConverter do
     converter.to_h.must_equal h
   end
 
+  it 'converts a dotted string key delimited by tab to an hash' do
+    str = "a.b\tc\n"
+    h = {"a" => {"b" => "c"}}
+    converter = I18n::Tools::StringConverter.new(str)
+    converter.to_h.must_equal h
+  end
+
+  it 'converts a dotted string key with an array as value to an hash' do
+    str = "a.b\t[\"c\", \"d\"]\n"
+    h = {"a" => {"b" => ["c", "d"]}}
+    converter = I18n::Tools::StringConverter.new(str)
+    converter.to_h.must_equal h
+  end
+
+  it 'converts a dotted string key with an array (not so well formatted) as value to an hash' do
+    str = "a.b\t[\"c\",\"d\"]\n"
+    h = {"a" => {"b" => ["c", "d"]}}
+    converter = I18n::Tools::StringConverter.new(str)
+    converter.to_h.must_equal h
+  end
+
 end
