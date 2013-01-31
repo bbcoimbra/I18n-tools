aeceaa8f2c8ddf68839dfcdd20cd6deaa8d680fe
Bruno Coimbra <bbcoimbra@gmail.com>
2013-01-30 23:09:03 -0200

    added initial dummy implementation of StringConverter

---
+++ lib/i18n/tools/string_converter.rb
@@ -0,0 +1,14 @@
+module I18n
+  module Tools
+    class StringConverter
+      def initialize(str)
+        @string = str
+      end
+
+      def to_h
+        {"a" => "b"}
+      end
+
+    end
+  end
+end
---
+++ test/unit/i18n/tools/string_converter_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+require 'i18n/tools/string_converter'
+
+describe I18n::Tools::StringConverter do
+  it 'converts a string delimited by tab to an hash' do
+    str = "a\tb\n"
+    h = {"a" => "b"}
+    converter = I18n::Tools::StringConverter.new(str)
+    converter.to_h.must_equal h
+  end
+end
